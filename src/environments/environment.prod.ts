18f23f904d9e7a9a11abb8ae47817ccd300a7bc4
JebinDanielR <jebinderil@gmail.com>
2026-03-19 14:34:01 +0530

    chatbot render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://thecrumbco-api.onrender.com',
-  chatApiUrl: 'PLACEHOLDER_FOR_PROD_CHAT_API_URL'
+  chatApiUrl: 'https://thecrumbco-chatbot.onrender.com'
 };
